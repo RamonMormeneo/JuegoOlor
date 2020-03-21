72428813cdb3920ddd45cfeaf619fda23bb88d17
Sergi G <s.gonzalezpons@gmail.com>
2020-03-21 13:21:42 +0100

    Main Menu y model del perrete

---
+++ Olor2.5D/Assets/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    public GameObject howToPlay;
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void play()
+    {
+
+    }
+}
